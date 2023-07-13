77f89d8bd512f928a09117fd2ede8b3b8cd5393d
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-07-13 11:14:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,6 +9,34 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/main.html'));
 });
 //your code here
+function rot13(str) {
+  let decoded = "";
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    // Check if the character is a letter
+    if (/[A-Z]/.test(char)) {
+      // Convert the character code to a value between 0 and 25
+      let charCode = char.charCodeAt(0) - 65;
+
+      // Apply the ROT13 shift (add 13 and wrap around if necessary)
+      let decodedCharCode = (charCode + 13) % 26;
+
+      // Convert the value back to a character code
+      let decodedChar = String.fromCharCode(decodedCharCode + 65);
+
+      // Append the decoded character to the result
+      decoded += decodedChar;
+    } else {
+      // Non-alphabetic character, pass it through unchanged
+      decoded += char;
+    }
+  }
+
+  return decoded;
+}
+
 app.post('/add', (req, res) => {
   const {a,b} = req.body;
   res.status(200).send(a+b);
